b8b5859e7aa3f5f1f8bfe707e0e497e4c2b9e42e
jack <javxlan@gmail.com>
2020-04-12 00:43:51 +0900

    day10,11 are done

---
+++ src/leetcode/Day10.java
@@ -0,0 +1,80 @@
+package leetcode;
+
+import javax.management.StandardEmitterMBean;
+
+public class Day10 {
+
+    class MinStack {
+
+        /**
+         * initialize your data structure here.
+         */
+
+        Node root = new Node(null);
+
+        public MinStack() {
+        }
+
+        public void push(int x) {
+            Node newNode = new Node(null);
+            newNode.val = x;
+            if (root.next == null) {
+                root.next = newNode;
+                root.next.min = x;
+            } else {
+                Node temp = root.next;
+                newNode.next = temp;
+                newNode.min = Math.min(x, root.next.min);
+                root.next = newNode;
+            }
+
+        }
+
+        public void pop() {
+            if (root.next != null) {
+                Node temp = root.next;
+                root.next = temp.next;
+            }
+        }
+
+        public int top() {
+            return root.next.val;
+        }
+
+        public int getMin() {
+            return root.next.min;
+        }
+    }
+
+    public class Node {
+        int val;
+        Node next;
+        int min;
+
+        public Node(Node next) {
+            this.next = next;
+        }
+    }
+}
+
+
+/**
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ * <p>
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ * <p>
+ * Example
+ * <p>
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ */
+
---
+++ src/leetcode/Day11.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class Day11 {
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+
+        if (root.left == null) return getDepthOfTree(root.right, 0);
+        if (root.right == null) return getDepthOfTree(root.left, 0);
+
+        return getDepthOfTree(root.left, 0) + getDepthOfTree(root.right, 0);
+    }
+
+
+    public int getDepthOfTree(TreeNode node, int depth) {
+
+        if (node.left != null) getDepthOfTree(node.left, depth + 1);
+        if (node.right != null) getDepthOfTree(node.right, depth + 1    );
+
+        return depth;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
