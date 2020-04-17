4800039482f674e12b9c28975538821cd32f5402
javxlan <javxlan@gmail.com>
2020-04-17 17:21:26 +0900

    day15 is done, and fixed problem of numbers

--- src/leetcode/Day13.java
+++ src/leetcode/Day13.java
@@ -1,53 +1,15 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Day13 {
-
     public static void main(String[] args) {
-        int[] stones = new int[]{2, 7, 4, 1, 8, 1};
 
-        System.out.println(new Day13().lastStoneWeight(stones));
+        int[] arr = new int[]{0, 1, 0};
+        System.out.println(new Day13().findMaxLength(arr));
+
     }
 
+    public int findMaxLength(int[] nums) {
 
-    public int lastStoneWeight(int[] stones) {
-
-        Arrays.sort(stones);
-        int size = stones.length;
-        if (size == 0) return 0;
-        if (size == 1) return stones[0];
-
-        int y = stones[size - 1];
-        int x = stones[size - 2];
-
-        stones[size - 1] = y - x;
-        stones[size - 2] = x - x;
-
-        return lastStoneWeight(updateStone(stones));
-    }
-
-    public int[] updateStone(int[] arr) {
-        List<Integer> updated = new ArrayList<>();
-        int size = arr.length;
-
-        for (int i = 0; i < size; i++) {
-            if (arr[i] != 0) updated.add(arr[i]);
-        }
-        return updated.stream().mapToInt(i -> i).toArray();
+        return 1;
     }
 }
-
-/**
- * Contiguous Array
- * Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
- * Input: [0,1]
- * Output: 2
- * Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
- * Input: [0,1,0]
- * Output: 2
- * Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
- */
-
--- src/leetcode/Day15.java
+++ src/leetcode/Day15.java
@@ -1,44 +1,30 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class Day15 {
     public static void main(String[] args) {
-        int[][] shift = new int[][]{{1, 1}, {1, 1}, {0, 2}, {1, 3}};
-        System.out.println(new Day15().stringShift("", shift));
+        int[] nums = new int[]{1, 2, 3, 4};
+        System.out.println(Arrays.toString(new Day15().productExceptSelf(nums)));
     }
 
-    public String stringShift(String s, int[][] shift) {
-        if (s == "") return s;
-        for (int[] arr : shift) {
-            s = shift(arr, s);
+    public int[] productExceptSelf(int[] nums) {
+        int size = nums.length;
+        int[] result = new int[size];
+        for (int i = 0; i < size; i++) {
+            int product = 1;
+            for (int j = 0; j < size; j++) {
+                if (i != j) {
+                    product *= nums[j];
+                }
+            }
+            result[i] = product;
         }
-        return s;
-    }
-
-    public String shift(int[] arr, String str) {
-        for (int i = 0; i < arr[1]; i++) {
-            if (arr[0] == 1) str = shiftLeft(str);
-            else str = shiftRight(str);
-        }
-        return str;
-    }
-
-    public String shiftRight(String str) {
-        str = str.substring(1, str.length()) + str.substring(0, 1);
-        return str;
-    }
-
-    public String shiftLeft(String str) {
-        str = str.substring(str.length() - 1, str.length()) + str.substring(0, str.length() - 1);
-        return str;
+        return result;
     }
 }
 
 /**
- * You are given a string s containing lowercase English letters, and a matrix shift, where shift[i] = [direction, amount]:
- * <p>
- * direction can be 0 (for left shift) or 1 (for right shift).
- * amount is the amount by which string s is to be shifted.
- * A left shift by 1 means remove the first character of s and append it to the end.
- * Similarly, a right shift by 1 means remove the last character of s and add it to the beginning.
- * Return the final string after all operations.
+ * Product of Array Except Self
+ * Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i]..
  */
