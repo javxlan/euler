5d41b77980c5920a467fab2ce646d67021877faf
javxlan <javxlan@gmail.com>
2020-04-24 13:36:26 +0900

    Day21 is done. and I got the Ladder Approach.

---
+++ src/leetcode/Day21.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class Day21 {
+    /*public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+        List<Integer> dim = binaryMatrix.dimensions();
+        int result = -1;
+        int x = dim[0] - 1;
+        int y = dim[1] - 1;
+
+        while (x >= 0 && y >= 0) {
+            if (binaryMatrix.get(x, y) == 1) {
+                result = y;
+                y -= 1;
+            } else {
+                x -= 1;
+            }
+        }
+
+        return result;
+    }*/
+}
