bc025616adf1cf84ddedf18a78d72483b8e2dd77
jack <javxlan@gmail.com>
2020-05-05 12:50:03 +0900

    Day4 is done.

---
+++ src/leetcode/may/RansomNote.java
@@ -0,0 +1,22 @@
+package leetcode.may;
+
+public class RansomNote {
+
+    public static void main(String[] args) {
+        System.out.println(new RansomNote().canConstruct("aa", "aab"));
+    }
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        char[] chars = ransomNote.toCharArray();
+        int total = 0;
+        for (char c : chars) {
+            int i = magazine.indexOf(c);
+            if (i >= 0) {
+                magazine = magazine.substring(0, i) + magazine.substring(i + 1, magazine.length());
+                total++;
+            }
+        }
+        return total == ransomNote.length();
+    }
+}
