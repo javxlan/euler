9dd2eff0a54ad7d05acda5f4a05900e52ff4c6bf
jack <javxlan@gmail.com>
2021-02-04 02:04:03 +0900

    added test data loader from hackerrank

--- src/hackerrank/Solution.java
+++ src/hackerrank/Solution.java
@@ -1,18 +1,73 @@
 package hackerrank;
 
-
-import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.net.URL;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    // Complete the riddle function below.
+    static long[] a;
 
-        Scanner s = new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {
-            int n = s.nextInt();
-            int x = n;
-            int y = (n & (n - 1));
-            System.out.println(x & y);
+    static long[] riddle(long[] arr) {
+        a = arr;
+        int n = a.length;
+        long[] ans = new long[n];
+        long[][] dp = new long[n][n];
+        // length
+        for (int i = 0; i < n; i++) {
+            long max = Long.MIN_VALUE;
+            for (int j = 0; j + i < n; j++) {
+                long min = a[j + i];
+                if (i == 0) {
+                    min = getMin(j, j + i);
+                } else {
+                    min = Math.min(min, dp[i - 1][j + i - 1]);
+                }
+                dp[i][j + i] = min;
+                max = Math.max(max, min);
+            }
+            ans[i] = max;
         }
+        return ans;
+    }
+
+    static long getMin(int s, int e) {
+        long min = Long.MAX_VALUE;
+        while (s <= e) {
+            min = Math.min(min, a[s]);
+            s++;
+        }
+        return min;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        //a = new long[]{"sdfs"};
+        URL url = new URL("https://hr-testcases-us-east-1.s3.amazonaws.com/70881/input04.txt?AWSAccessKeyId=AKIAR6O7GJNX5DNFO3PV&Expires=1612375988&Signature=zbwVl2A21WtNEFB3%2BFbFeyivqpg%3D&response-content-type=text%2Fplain");
+        BufferedReader read = new BufferedReader(
+                new InputStreamReader(url.openStream()));
+        String i;
+        int line = 0;
+        while ((i = read.readLine()) != null) {
+
+            if (line == 0) {
+                a = new long[Integer.parseInt(i)];
+            } else {
+//                a = Arrays.stream(i.split(" "))
+//                        .mapToLong(Long::parseLong)
+//                        .toArray();
+            }
+            line++;
+        }
+        read.close();
+
+        System.out.println(Arrays.toString(riddle(a)));
     }
 }
+
