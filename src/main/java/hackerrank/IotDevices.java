fe4a1046c8fcf4f873a820382da686b086f532df
jack <javxlan@gmail.com>
2021-04-04 23:34:09 +0900

    added hackerrank Rest API

--- src/main/java/hackerrank/IotDevices.java
+++ src/main/java/hackerrank/IotDevices.java
@@ -1,16 +1,70 @@
 package hackerrank;
 
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 public class IotDevices {
     public static void main(String[] args) {
+        String status = "STOPPED";
+        String dateStr = "03-2019";
 
-        //date.setTime(ts.getTime());
-        String formattedDate = new SimpleDateFormat("MM-YYYY").format(Long.parseLong("1552799232246"));
-        System.out.println(formattedDate);
+        IotDevices p = new IotDevices();
+        System.out.println(p.countDevices(status, dateStr, 45));
+    }
+
+    public int countDevices(String status, String dateStr, int threshold) {
+        int count = 0;
+        String url = "https://jsonmock.hackerrank.com/api/iot_devices/search?status=" + status;
+        JsonObject response = getUrl(url);
+        int pages = response.get("total_pages").getAsInt();
+        for (int i = 1; i <= pages; i++) {
+            JsonObject page = getUrl(url + "&page=" + i);
+            JsonArray arr = page.getAsJsonArray("data");
+            for (int j = 0; j < arr.size(); j++) {
+
+                JsonObject row = arr.get(i).getAsJsonObject();
+                String date = row.get("timestamp").getAsString();
+                String converted = timeStampToString(date);
+                if (row.getAsJsonObject("operatingParams").get("rootThreshold").getAsInt() > threshold && converted.equals(dateStr)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
+    public String timeStampToString(String timestamp) {
+        String formattedDate = new SimpleDateFormat("MM-YYYY").format(Long.parseLong(timestamp));
+        return formattedDate;
+    }
+
+    public JsonObject getUrl(String url) {
+        var client = HttpClient.newHttpClient();
+        var httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+        try {
+            var response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            JsonObject obj = new JsonParser().parse(response.body()).getAsJsonObject();
+            return obj;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
--- src/main/java/interview/KidsLoopTest.java
+++ src/main/java/interview/KidsLoopTest.java
@@ -1,11 +1,12 @@
 package interview;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author Javkhlan Gansukh
  */
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class KidsLoopTest {
 
@@ -13,20 +14,20 @@ class KidsLoopTest {
     void solve() {
         KidsLoop test = new KidsLoop();
         test.threshold = 8;
-        assertEquals(145, test.solve());
+        Assert.assertEquals(145, test.solve());
     }
 
     @Test
     void solveWhenNegativeThreshold() {
         KidsLoop test = new KidsLoop();
         test.threshold = -1;
-        assertEquals(0, test.solve());
+        Assert.assertEquals(0, test.solve());
     }
 
     @Test
     void solveWhenDefaultThreshold() {
         KidsLoop test = new KidsLoop();
-        assertEquals(592597, test.solve());
+        Assert.assertEquals(592597, test.solve());
     }
 
     @Test
@@ -34,14 +35,14 @@ class KidsLoopTest {
         KidsLoop test = new KidsLoop();
         Point p = new Point(1, 2);
         test.visited.put(String.valueOf(p.x) + "," + String.valueOf(p.y), p);
-        assertEquals(true, test.isVisited(p));
+        Assert.assertEquals(true, test.isVisited(p));
     }
 
     @Test
     void isNotVisited() {
         KidsLoop test = new KidsLoop();
         Point p = new Point(1, 2);
-        assertEquals(false, test.isVisited(p));
+        Assert.assertEquals(false, test.isVisited(p));
     }
 
     @Test
@@ -49,7 +50,7 @@ class KidsLoopTest {
         KidsLoop test = new KidsLoop();
         test.threshold = 5;
         Point p = new Point(1, 2);
-        assertEquals(true, test.isSafe(p));
+        Assert.assertEquals(true, test.isSafe(p));
     }
 
     @Test
@@ -57,18 +58,18 @@ class KidsLoopTest {
         KidsLoop test = new KidsLoop();
         test.threshold = 5;
         Point p = new Point(5, 4);
-        assertEquals(false, test.isSafe(p));
+        Assert.assertEquals(false, test.isSafe(p));
     }
 
     @Test
     void digitSumNegativePoint() {
         KidsLoop test = new KidsLoop();
-        assertEquals(9, test.digitSum(-153));
+        Assert.assertEquals(9, test.digitSum(-153));
     }
 
     @Test
     void digitSum() {
         KidsLoop test = new KidsLoop();
-        assertEquals(12, test.digitSum(75));
+        Assert.assertEquals(12, test.digitSum(75));
     }
 }
